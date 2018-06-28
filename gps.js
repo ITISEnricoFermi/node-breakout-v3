d0f0a566799a53a9095492687c54f377c771a0a3
Hristo Kamenov <hkamenov123@gmail.com>
2018-06-28 00:44:55 +0000

    Initial commit

---
+++ gps.js
@@ -0,0 +1,26 @@
+class GPS {
+    constructor(port) {
+        this.portName = port
+        this.SerialPort = require('serialport')
+        this.NodeNMEA = require('node-nmea')
+    }
+
+    start() {
+        this.port = new this.SerialPort(this.portName, {baudRate: 9600})
+        this.parser = this.port.pipe(new this.SerialPort.parsers.Readline({ delimiter: '\r\n'}))
+        this.parser.on('data', (data) => {
+            this.lastState = this.NodeNMEA.parse(data)
+        })
+    }
+
+    stop() {
+        this.port = null
+        this.lastState = null
+    }
+
+    getData() {
+        return this.lastState
+    }
+}
+
+module.exports = GPS
